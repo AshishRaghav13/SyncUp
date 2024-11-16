b76327c77a4384dfe35ae69d6f91e51605878f19
Ashish Raghav <ashishraghav2468@gmail.com>
2024-11-16 16:38:51 +0530

    Exploring Http Methods

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server");
+// this will only handle get calls to /users
+//can also use ?,+,* in between the routes 
+app.get("/user",(req,res)=>{
+    res.send({username : "Ashish",age:22});
 })
 
-app.use("/",(req,res)=>{
-    res.send("hii ashish");
+app.post("/user",(req,res)=>{
+    res.send("Successsfully stored data in the DB");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
+})
+
+app.put("/user",(req,res)=>{
+    res.send("completed all")
+})
+
+
+// this will match all the HTTP method API calls
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server");
 })
 
 app.listen(3000,()=>{
